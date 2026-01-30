73e0b71fba279c2e6b51d1115d72b0ced32193b7
Waheed <wsulimankhail@gmail.com>
2026-01-30 13:00:49 +0100

    [TASK] soleil yp and soleil tune device name based on the Workshop based twin

--- src/accml_lib/custom/soleil/manager_setup.py
+++ src/accml_lib/custom/soleil/manager_setup.py
@@ -57,17 +57,20 @@ def load_managers() -> Tuple[
     YellowPagesBase, LiaisonManagerBase, TranslatorServiceBase
 ]:
     quad_names = [
-        f"AN01-AR/EM/{id_}" for id_ in ("QF01.01", "QF03.03", "QF05.06", "QF06.05")
+        f"AN01-AR/EM-QP/{id_}" for id_ in ("QF01.01", "QF03.03", "QF05.06", "QF06.05")
     ]
-    quad_names += [f"AN01-AR/EM/{id_}" for id_ in ("QD02.02", "QD04.07", "QD07.04")]
-
+    quad_names += [f"AN01-AR/EM-QP/{id_}" for id_ in ("QD02.02", "QD04.07", "QD07.04")]
+    quad_names += [f"AN02-AR/EM-QP/{id_}" for id_ in ("QD04.01", "QD07.04", "QD08.08", "QD11.05", "QF05.02", "QF06.03", "QF09.07", "QF10.06")]
+    quad_names += [f"AN03-AR/EM-QP/{id_}" for id_ in ("QD08.01", "QD08.08", "QD11.04", "QD11.05", "QF09.02", "QF09.07", "QF10.03", "QF10.06")]
+    quad_names += [f"AN04-AR/EM-QP/{id_}" for id_ in ("QD08.01", "QD12.08", "QD11.04", "QD15.05", "QF09.02", "QF09.03", "QF13.07", "QF14.06")]
+    quad_names += [f"AN05-AR/EM-QP/{id_}" for id_ in ("QD12.01", "QD15.04", "QD18.08", "QD21.05", "QF13.02", "QF14.03", "QF19.07", "QF20.06")]
     quad_ids = [TRL.from_trl(name) for name in quad_names]
     yp = YellowPages(
         dict(
             quadrupoles=quad_ids,
             # Todo: need to select the correct quad
             tune_correction_quadrupoles=quad_ids,
-            tune=[TRL("PHYSICS", "SOLEIL", "TUNE")],
+            tune=[TRL("simulator", "ringsimulator", "ringsimulator")],
         )
     )
     return yp, None, None
